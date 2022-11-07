ad319b3f28697b7e693febc5fba9e5896ed16534
JonahEgashira <jonahega@gmail.com>
2022-11-07 20:02:05 +0900

    Update

--- clean.sh
+++ clean.sh
@@ -5,13 +5,3 @@ rm *.pdf
 rm ./build/*.log
 rm ./build/*.aux
 rm ./build/*.pdf
-
-rm ./sections/*.aux
-rm ./sections/*.log
-rm ./sections/*.pdf
-
-rm ./sections/build/*.aux
-rm ./sections/build/*.log
-rm ./sections/build/*.pdf
-
-rm -rf ./build/sections
