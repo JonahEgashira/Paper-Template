8a6c6b233ee7ce366ec25cb956ddb268d1ba4007
JonahEgashira <jonahega@gmail.com>
2022-11-07 19:37:32 +0900

    Initial commit

---
+++ clean.sh
@@ -0,0 +1,15 @@
+rm *.aux
+rm *.log
+rm *.pdf
+
+rm ./build/*.log
+rm ./build/*.aux
+rm ./build/*.pdf
+
+rm ./sections/*.aux
+rm ./sections/*.log
+rm ./sections/*.pdf
+
+rm ./sections/build/*.aux
+rm ./sections/build/*.log
+rm ./sections/build/*.pdf
